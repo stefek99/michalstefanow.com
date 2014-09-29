4b7d9a008ccaa1f87c4d5b7420fd44929add5242
Michal Stefanow <mstefanow@gmail.com>
2014-09-29 14:32:45 +0100

    cleanup of the master branch (everything on gh-pages)

--- __OLD-20140110_2/network/_auth/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (isset($_POST['stuff']))  {
-		$stuff = $_POST['stuff'];
-
-		$fh = fopen($myFile, 'w') or die("can't open file");
-		fwrite($fh, $stuff);
-		fclose($fh);
-	}
-?>
---
+++ readme.md
@@ -0,0 +1,1 @@
+Use **gh-pages** branch.
